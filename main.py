1cb4d8082f4806fad5ba7163e9454ceb0b15455c
Noah Reinalter <38698485+noahreinalter@users.noreply.github.com>
2021-04-29 00:43:35 +0200

    Added the possibility to add and remove the member role and to delete a generated channel category
    Signed-off-by: Noah Reinalter <38698485+noahreinalter@users.noreply.github.com>

--- main.py
+++ main.py
@@ -23,8 +23,23 @@ class BotClient(discord.Client):
         if message.author.bot:
             return
 
-        if message.content.startswith('$generate'):
+        text = message.content
+
+        # TODO: Add good help message
+        if text.startswith('$help'):
+            await message.channel.send()
+
+        if text.startswith('$generate'):
             await generation_controller(message)
+
+        if text.startswith('$delete'):
+            await delete_controller(message)
+
+        if text.startswith('$add'):
+            await add_member_to_role(message)
+
+        if text.startswith('$remove'):
+            await remove_member_from_role(message)
 
 
 async def generation_controller(message):
@@ -37,20 +52,6 @@ async def generation_controller(message):
     await generate_channels(message.guild, role_name, roles)
 
     await message.channel.send('New roles and channels generated.')
-
-
-async def generate_channels(guild, channel_name, roles):
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        roles[0]: discord.PermissionOverwrite(read_messages=True),
-        roles[1]: discord.PermissionOverwrite(read_messages=True)
-    }
-    category = await guild.create_category(name=channel_name, overwrites=overwrites)
-
-    await asyncio.gather(
-        guild.create_text_channel(name=channel_name + '-text', category=category, sync_permissions=True),
-        guild.create_voice_channel(name=channel_name + '-voice', category=category, sync_permissions=True)
-    )
 
 
 async def generate_roles(guild, role_name, member):
@@ -69,6 +70,66 @@ async def generate_roles(guild, role_name, member):
     )
 
     return roles
+
+
+async def generate_channels(guild, channel_name, roles):
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        roles[0]: discord.PermissionOverwrite(read_messages=True),
+        roles[1]: discord.PermissionOverwrite(read_messages=True)
+    }
+    category = await guild.create_category(name=channel_name, overwrites=overwrites)
+
+    await asyncio.gather(
+        guild.create_text_channel(name=channel_name + '-text', category=category, sync_permissions=True),
+        guild.create_voice_channel(name=channel_name + '-voice', category=category, sync_permissions=True)
+    )
+
+
+async def delete_controller(message):
+    if discord.utils.get(message.author.roles, name='Admin') is None:
+        await message.channel.send('Only User with the role Admin use this command.')
+        return
+    role_name = message.content.split()[1]
+    await delete_roles(message.guild, role_name)
+
+    await delete_channels(message.guild, role_name)
+
+    await message.channel.send('Roles and channels have been deleted.')
+
+
+async def delete_roles(guild, role_name):
+    await asyncio.gather(
+        discord.utils.get(guild.roles, name=role_name + ' Leader').delete(),
+        discord.utils.get(guild.roles, name=role_name + ' Member').delete()
+    )
+
+
+async def delete_channels(guild, role_name):
+    category_channel = discord.utils.get(guild.channels, name=role_name)
+    await asyncio.gather(
+        category_channel.channels[0].delete(),
+        category_channel.channels[1].delete()
+    )
+    await category_channel.delete()
+
+
+async def add_member_to_role(message):
+    leader_role = message.role_mentions[0].name.split()[0] + ' Leader'
+    if discord.utils.get(message.author.roles, name=leader_role):
+        await message.mentions[0].add_roles(message.role_mentions[0])
+        await message.channel.send('Role added to user.')
+    else:
+        await message.channel.send('To add this role you need to have the role ' + leader_role + '.')
+
+
+async def remove_member_from_role(message):
+    leader_role = message.role_mentions[0].name.split()[0] + ' Leader'
+    if discord.utils.get(message.author.roles, name=leader_role):
+        await message.mentions[0].remove_roles(message.role_mentions[0])
+        await message.channel.send('Role removed from user.')
+    else:
+        await message.channel.send('To remove this role you need to have the role ' + leader_role + '.')
 
 
 if __name__ == '__main__':
