7a00101ab1dd8e595b97e66c73c752083d436305
Noah Reinalter <38698485+noahreinalter@users.noreply.github.com>
2021-05-01 16:16:38 +0200

    Changed structure to use extensions
    Signed-off-by: Noah Reinalter <38698485+noahreinalter@users.noreply.github.com>

---
+++ category_generator.py
@@ -0,0 +1,163 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+from main import prefix, leader_string, member_string
+
+
+def setup(bot):
+    bot.add_cog(CategoryGenerator(bot))
+    print('CategoryGenerator is being loaded!')
+
+
+def teardown(bot):
+    bot.remove_cog('CategoryGenerator')
+    print('CategoryGenerator is being unloaded')
+
+
+class CategoryGenerator(commands.Cog, name='Category Generator'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='generate', help='$generate categoryname @User')
+    @commands.has_role('Admin')
+    @commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
+                                        read_messages=True, send_messages=True)
+    @commands.guild_only()
+    async def generation_controller(self, ctx, category_name, member: commands.MemberConverter):
+        if discord.utils.get(ctx.guild.channels, name=category_name) is None:
+            roles = await self.generate_roles(ctx.guild, category_name, member)
+
+            await self.generate_channels(ctx.guild, category_name, roles)
+
+            await ctx.send('New roles and channels generated.')
+        else:
+            await ctx.send('There are already channels in this namespace.')
+
+    @generation_controller.error
+    async def generation_controller_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send('This command only works on a server.')
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
+                           + prefix + 'invite')
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.send('Only User with the role Admin use this command.')
+        else:
+            await ctx.send('Something went wrong pleas try again.')
+
+    async def generate_roles(self, guild, role_name, member):
+        await asyncio.gather(
+            guild.create_role(name=role_name + leader_string),
+            guild.create_role(name=role_name + member_string)
+        )
+
+        all_roles = await guild.fetch_roles()
+        roles = [discord.utils.get(all_roles, name=role_name + leader_string),
+                 discord.utils.get(all_roles, name=role_name + member_string)]
+
+        await asyncio.gather(
+            member.add_roles(roles[0]),
+            member.add_roles(roles[1])
+        )
+
+        return roles
+
+    async def generate_channels(self, guild, channel_name, roles):
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            roles[0]: discord.PermissionOverwrite(read_messages=True),
+            roles[1]: discord.PermissionOverwrite(read_messages=True),
+            discord.utils.get(guild.roles, name=self.bot.user.name): discord.PermissionOverwrite(read_messages=True)
+        }
+        category = await guild.create_category(name=channel_name, overwrites=overwrites)
+
+        await asyncio.gather(
+            guild.create_text_channel(name=channel_name + '-text', category=category, sync_permissions=True),
+            guild.create_voice_channel(name=channel_name + '-voice', category=category, sync_permissions=True)
+        )
+
+    @commands.command(name='delete', help='$delete categoryname')
+    @commands.has_role('Admin')
+    @commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
+                                        read_messages=True, send_messages=True)
+    @commands.guild_only()
+    async def delete_controller(self, ctx, category_name):
+        await self.delete_roles(ctx.guild, category_name)
+        await self.delete_channels(ctx.guild, category_name)
+
+        await ctx.send('Roles and channels have been deleted.')
+
+    @delete_controller.error
+    async def delete_controller_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send('This command only works on a server.')
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
+                           + prefix + 'invite')
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.send('Only User with the role Admin use this command.')
+        else:
+            await ctx.send('Something went wrong pleas try again.')
+
+    async def delete_roles(self, guild, role_name):
+        await asyncio.gather(
+            discord.utils.get(guild.roles, name=role_name + leader_string).delete(),
+            discord.utils.get(guild.roles, name=role_name + member_string).delete()
+        )
+
+    async def delete_channels(self, guild, role_name):
+        category_channel = discord.utils.get(guild.channels, name=role_name)
+        await asyncio.gather(
+            category_channel.channels[0].delete(),
+            category_channel.channels[1].delete()
+        )
+        await category_channel.delete()
+
+    @commands.command(name='add', help='$add @User @Role')
+    @commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
+                                        read_messages=True, send_messages=True)
+    @commands.guild_only()
+    async def add_member_to_role(self, ctx, member: commands.MemberConverter, role: commands.RoleConverter):
+        leader_role = role.name.split()[0] + leader_string
+        if discord.utils.get(ctx.author.roles, name=leader_role) is None:
+            raise commands.MissingRole(leader_role)
+
+        await member.add_roles(discord.utils.get(ctx.guild.roles, name=role.name.split()[0] + member_string))
+        await ctx.send('Role added to user.')
+
+    @add_member_to_role.error
+    async def add_member_to_role_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send('This command only works on a server.')
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
+                           + prefix + 'invite')
+        elif isinstance(error, commands.MissingRole):
+            await ctx.send('To add this role you need to have the role ' + error.missing_role + '.')
+        else:
+            await ctx.send('Something went wrong pleas try again.')
+
+    @commands.command(name='remove', help='$remove @User @Role')
+    @commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
+                                        read_messages=True, send_messages=True)
+    @commands.guild_only()
+    async def remove_member_from_role(self, ctx, member: commands.MemberConverter, role: commands.RoleConverter):
+        leader_role = role.name.split()[0] + leader_string
+        if discord.utils.get(ctx.author.roles, name=leader_role) is None:
+            raise commands.MissingRole(leader_role)
+
+        await member.remove_roles(discord.utils.get(ctx.guild.roles, name=role.name.split()[0] + member_string))
+        await ctx.send('Role removed from user.')
+
+    @remove_member_from_role.error
+    async def remove_member_from_role_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send('This command only works on a server.')
+        elif isinstance(error, commands.BotMissingPermissions):
+            await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
+                           + prefix + 'invite')
+        elif isinstance(error, commands.MissingRole):
+            await ctx.send('To remove this role you need to have the role ' + error.missing_role + '.')
+        else:
+            await ctx.send('Something went wrong pleas try again.')
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import logging
 import os
 from dotenv import load_dotenv
-import asyncio
+import sqlite3
+from sqlite3 import Error
 
 load_dotenv()
 
@@ -11,6 +12,8 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 prefix = '$'
 leader_string = ' Leader'
 member_string = ' Member'
+sql_path = 'db.sqlite'
+sql_connection = None
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -24,161 +27,6 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
-
-
-@bot.command(name='generate', help='$generate categoryname @User')
-@commands.has_role('Admin')
-@commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
-                                    read_messages=True, send_messages=True)
-@commands.guild_only()
-async def generation_controller(ctx, category_name, member: commands.MemberConverter):
-    if discord.utils.get(ctx.guild.channels, name=category_name) is None:
-        roles = await generate_roles(ctx.guild, category_name, member)
-
-        await generate_channels(ctx.guild, category_name, roles)
-
-        await ctx.send('New roles and channels generated.')
-    else:
-        await ctx.send('There are already channels in this namespace.')
-
-
-@generation_controller.error
-async def generation_controller_error(ctx, error):
-    if isinstance(error, commands.NoPrivateMessage):
-        await ctx.send('This command only works on a server.')
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
-                       + prefix + 'invite')
-    elif isinstance(error, commands.CheckFailure):
-        await ctx.send('Only User with the role Admin use this command.')
-    else:
-        await ctx.send('Something went wrong pleas try again.')
-
-
-async def generate_roles(guild, role_name, member):
-    await asyncio.gather(
-        guild.create_role(name=role_name + leader_string),
-        guild.create_role(name=role_name + member_string)
-    )
-
-    all_roles = await guild.fetch_roles()
-    roles = [discord.utils.get(all_roles, name=role_name + leader_string),
-             discord.utils.get(all_roles, name=role_name + member_string)]
-
-    await asyncio.gather(
-        member.add_roles(roles[0]),
-        member.add_roles(roles[1])
-    )
-
-    return roles
-
-
-async def generate_channels(guild, channel_name, roles):
-    overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        roles[0]: discord.PermissionOverwrite(read_messages=True),
-        roles[1]: discord.PermissionOverwrite(read_messages=True),
-        discord.utils.get(guild.roles, name=bot.user.name): discord.PermissionOverwrite(read_messages=True)
-    }
-    category = await guild.create_category(name=channel_name, overwrites=overwrites)
-
-    await asyncio.gather(
-        guild.create_text_channel(name=channel_name + '-text', category=category, sync_permissions=True),
-        guild.create_voice_channel(name=channel_name + '-voice', category=category, sync_permissions=True)
-    )
-
-
-@bot.command(name='delete', help='$delete categoryname')
-@commands.has_role('Admin')
-@commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
-                                    read_messages=True, send_messages=True)
-@commands.guild_only()
-async def delete_controller(ctx, category_name):
-    await delete_roles(ctx.guild, category_name)
-    await delete_channels(ctx.guild, category_name)
-
-    await ctx.send('Roles and channels have been deleted.')
-
-
-@delete_controller.error
-async def delete_controller_error(ctx, error):
-    if isinstance(error, commands.NoPrivateMessage):
-        await ctx.send('This command only works on a server.')
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
-                       + prefix + 'invite')
-    elif isinstance(error, commands.CheckFailure):
-        await ctx.send('Only User with the role Admin use this command.')
-    else:
-        await ctx.send('Something went wrong pleas try again.')
-
-
-async def delete_roles(guild, role_name):
-    await asyncio.gather(
-        discord.utils.get(guild.roles, name=role_name + leader_string).delete(),
-        discord.utils.get(guild.roles, name=role_name + member_string).delete()
-    )
-
-
-async def delete_channels(guild, role_name):
-    category_channel = discord.utils.get(guild.channels, name=role_name)
-    await asyncio.gather(
-        category_channel.channels[0].delete(),
-        category_channel.channels[1].delete()
-    )
-    await category_channel.delete()
-
-
-@bot.command(name='add', help='$add @User @Role')
-@commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
-                                    read_messages=True, send_messages=True)
-@commands.guild_only()
-async def add_member_to_role(ctx, member: commands.MemberConverter, role: commands.RoleConverter):
-    leader_role = role.name.split()[0] + leader_string
-    if discord.utils.get(ctx.author.roles, name=leader_role) is None:
-        raise commands.MissingRole(leader_role)
-
-    await member.add_roles(discord.utils.get(ctx.guild.roles, name=role.name.split()[0] + member_string))
-    await ctx.send('Role added to user.')
-
-
-@add_member_to_role.error
-async def add_member_to_role_error(ctx, error):
-    if isinstance(error, commands.NoPrivateMessage):
-        await ctx.send('This command only works on a server.')
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
-                       + prefix + 'invite')
-    elif isinstance(error, commands.MissingRole):
-        await ctx.send('To add this role you need to have the role ' + error.missing_role + '.')
-    else:
-        await ctx.send('Something went wrong pleas try again.')
-
-
-@bot.command(name='remove', help='$remove @User @Role')
-@commands.bot_has_guild_permissions(manage_channels=True, manage_roles=True,
-                                    read_messages=True, send_messages=True)
-@commands.guild_only()
-async def remove_member_from_role(ctx, member: commands.MemberConverter, role: commands.RoleConverter):
-    leader_role = role.name.split()[0] + leader_string
-    if discord.utils.get(ctx.author.roles, name=leader_role) is None:
-        raise commands.MissingRole(leader_role)
-
-    await member.remove_roles(discord.utils.get(ctx.guild.roles, name=role.name.split()[0] + member_string))
-    await ctx.send('Role removed from user.')
-
-
-@remove_member_from_role.error
-async def remove_member_from_role_error(ctx, error):
-    if isinstance(error, commands.NoPrivateMessage):
-        await ctx.send('This command only works on a server.')
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.send('The bot needs at least all permissions requested from the invite link generated with '
-                       + prefix + 'invite')
-    elif isinstance(error, commands.MissingRole):
-        await ctx.send('To remove this role you need to have the role ' + error.missing_role + '.')
-    else:
-        await ctx.send('Something went wrong pleas try again.')
 
 
 @bot.command(name='invite', help='Returns a invite link for the bot.')
@@ -200,5 +48,73 @@ async def stop_bot_error(ctx, error):
     pass
 
 
+@bot.command(name='add_extension', hidden=True)
+@commands.is_owner()
+async def add_extension(ctx, extension_name):
+    bot.load_extension(extension_name)
+    await ctx.send('Extension ' + extension_name + ' added to bot.')
+
+
+@add_extension.error
+async def add_extension_error(ctx, error):
+    pass
+
+
+@bot.command(name='remove_extension', hidden=True)
+@commands.is_owner()
+async def remove_extension(ctx, extension_name):
+    bot.unload_extension(extension_name)
+    await ctx.send('Extension ' + extension_name + ' removed from bot.')
+
+
+@remove_extension.error
+async def remove_extension_error(ctx, error):
+    pass
+
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+def execute_create_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def create_servers(connection):
+    execute_create_query(connection,
+                         """
+                            CREATE TABLE IF NOT EXISTS servers (
+                            id INTEGER PRIMARY KEY,
+                            prefix TEXT NOT NULL,
+                            admin_role Text
+                            );
+                        """)
+
+
+def create_extensions(connection):
+    execute_create_query(connection,
+                         """
+                            CREATE TABLE IF NOT EXISTS extensions (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT NOT NULL 
+                            );
+                        """)
+
+
 if __name__ == '__main__':
+    sql_connection = create_connection(sql_path)
+    create_servers(sql_connection)
+    create_extensions(sql_connection)
     bot.run(BOT_TOKEN)
